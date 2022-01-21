5335592609a4501584d71dc100733e134568de9e
Arie Timmerman <arietimmerman@gmail.com>
2022-01-21 22:40:48 +0100

    fixes and updated packages (#9)

--- routes/login/api.php
+++ routes/login/api.php
@@ -6,7 +6,7 @@ Route::group(['domain'=>'{tenant}.' . config('app.domain')], function ()
     \ArieTimmerman\Laravel\AuthChain\Providers\RouteProvider::routesApi();
 
 
-    Route::get('/language/{locale}','LanguageController@get')->name('ice.login.sessions');
+    Route::get('/language/{locale}','LanguageController@get');
 
     Route::get('/uiSettings','TenantSettingController@uiSettings');
 
