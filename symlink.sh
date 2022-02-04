ffb749a8aa0763ea16cb1227b097b64fca741b4b
Arie Timmerman <arietimmerman@gmail.com>
2022-02-04 22:18:03 +0100

    update

--- symlink.sh
+++
@@ -1,5 +0,0 @@
-ln -s /Users/arie/Documents/git/laravel-authchain laravel-authchain
-ln -s /Users/arie/Documents/git/laravel-authchain-facebook laravel-authchain-facebook
-ln -s /Users/arie/Documents/git/laravel-oidc passport
-ln -s /Users/arie/Documents/git/laravel-scim-server laravel-scim-server
-ln -s /Users/arie/Documents/git/samlidp laravel-saml
