11d6ffe36a845611e6ef5bb9ccb1953789389aa9
Arie Timmerman <arietimmerman@gmail.com>
2022-09-07 22:32:09 +0200

    fixes

--- routes/login/api.php
+++ routes/login/api.php
@@ -3,7 +3,7 @@
 Route::group(['domain'=>'{tenant}.' . config('app.domain')], function ()
 {
     
-    \ArieTimmerman\Laravel\AuthChain\Providers\RouteProvider::routesApi();
+    \App\AuthChain\Providers\RouteProvider::routesApi();
 
 
     Route::get('/language/{locale}','LanguageController@get');
