c24c4652e7cff97d749138697b96b55b2b4fef73
Arie Timmerman <arietimmerman@gmail.com>
2022-10-10 07:29:40 +0200

    removed file

--- init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-eval $(minikube docker-env)
-minikube start --mount-string="$(pwd):/var/www/html" --mount
-env $(cat .env.demo | xargs) docker-compose build laravel.test
-env $(cat .env.demo | xargs) docker-compose build node.test
-minikube kubectl apply -- -f ./kubernetes/
