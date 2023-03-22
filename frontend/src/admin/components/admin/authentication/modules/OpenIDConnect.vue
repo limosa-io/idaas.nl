f9a39cf2b95a36857d14e0175e9e692cd26ec9ec
Arie Timmerman <arietimmerman@gmail.com>
2023-03-22 21:56:08 +0100

    fixes oidc module creation (#42)

--- frontend/src/admin/components/admin/authentication/modules/OpenIDConnect.vue
+++ frontend/src/admin/components/admin/authentication/modules/OpenIDConnect.vue
@@ -11,28 +11,28 @@
     <b-form-input type="text" id="module.config.client_secret" v-model="module.config.client_secret" />
   </b-form-group>
 
-  <b-form-group horizontal :label-cols="3" description="Scopes." label="scopes" label-for="module.config.scopes">
+  <b-form-group horizontal :label-cols="3" description="list of the OAuth 2.0 &quot;scope&quot; values that this authorization server supports" label="scopes" label-for="module.config.scopes">
     <b-form-input type="text" id="module.config.scopes" v-model="module.config.scopes" />
   </b-form-group>
 
-  <b-form-group horizontal :label-cols="3" description="Userinfo endpoint." label="userinfo" label-for="module.config.userinfo_endpoint">
+  <b-form-group horizontal :label-cols="3" description="Userinfo endpoint." label="Userinfo endpoint" label-for="module.config.userinfo_endpoint">
     <b-form-input type="text" id="module.config.userinfo_endpoint" v-model="module.config.userinfo_endpoint" />
   </b-form-group>
 
-  <b-form-group horizontal :label-cols="3" description="authorization_endpoint." label="authorization_endpoint"
+  <b-form-group horizontal :label-cols="3" description="URL of the authorization server's authorization endpoint." label="Authorization Endpoint"
     label-for="module.config.authorization_endpoint">
     <b-form-input type="text" id="module.config.authorization_endpoint" v-model="module.config.authorization_endpoint" />
   </b-form-group>
 
-  <b-form-group horizontal :label-cols="3" description="token_endpoint." label="token_endpoint" label-for="module.config.token_endpoint">
+  <b-form-group horizontal :label-cols="3" description="URL of the authorization server's token endpoint." label="Token Endpoint" label-for="module.config.token_endpoint">
     <b-form-input type="text" id="module.config.token_endpoint" v-model="module.config.token_endpoint" />
   </b-form-group>
 
-  <b-form-group horizontal :label-cols="3" description="color." label="color" label-for="module.config.button_color">
+  <b-form-group horizontal :label-cols="3" description="Color of the login button." label="Color" label-for="module.config.button_color">
     <b-form-input type="text" id="module.config.button_color" v-model="module.config.button_color" />
   </b-form-group>
 
-  <b-form-group horizontal :label-cols="3" description="text." label="text" label-for="module.config.button_text">
+  <b-form-group horizontal :label-cols="3" description="Label for the login button" label="Label" label-for="module.config.button_text">
     <b-form-input type="text" id="module.config.button_text" v-model="module.config.button_text" />
   </b-form-group>
 
